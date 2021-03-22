968f266137c5a841a92ffe78ce29a8d1582dc5f9
Andres Auais <andresauais@gmail.com>
2021-03-22 21:31:44 +0000

    done

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -2,21 +2,19 @@ import React, { Component } from "react";
 
 import Button from "./Button";
 
-class ProductCard extends Component {
-  render() {
-    const { img, title, price, handleAddToCart } = this.props;
 
-    return (
-      <div className="col mb-4 d-flex flex-column product__card">
-        <img className="product__img" src={img} alt="" />
-        <div className="d-block">
-          <h3 className="h5">{title}</h3>
-          <p>{price}€</p>
-          <Button onClick={handleAddToCart}>Add to cart</Button>
-        </div>
+function ProductCard({ img, title, price, handleAddToCart }){
+  return (
+    <div className="col mb-4 d-flex flex-column product__card">
+      <img className="product__img" src={img} alt="" />
+      <div className="d-block">
+        <h3 className="h5">{title}</h3>
+        <p>{price}€</p>
+        <Button onClick={handleAddToCart}>Add to cart</Button>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
 
 export default ProductCard;
